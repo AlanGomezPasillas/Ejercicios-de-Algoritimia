4b2cb57c0a21afc022a72bdf3f87425bf068f1c5
AlanGomezPasillas <alangomezpasillas@gmail.com>
2025-02-20 17:52:38 -0600

    Update main.cpp

--- A01/c.-estructurado-iteracion/8.-JUEGO_ADIVINAR/While/main.cpp
+++ A01/c.-estructurado-iteracion/8.-JUEGO_ADIVINAR/While/main.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
 
+int main(){
+	int num, x, att;
+	bool ganaste = false;
+	att = 10;
+	srand(time(nullptr));
+	x = rand() % 100 + 1;
+	//cout<<"rnum = "<<x<<endl;
+
+	cout<<"ADIVINA EL NUMERO\n\n";
+	while(att--){	
+		cout<<"INTENTO #"<<att<<endl;
+		cout<<"Elige un numero del 1 al 100: ";
+		cin>>num;
+		if(num==x){
+			ganaste = true;
+			break;
+		}
+		cout<<"Ese no es...\n\n";
+	}
+	
+	ganaste?cout<<"Has Ganado!!!"<<endl:
+		cout<<"Perdiste =c"<<endl;
+
+	return 0;
+}
