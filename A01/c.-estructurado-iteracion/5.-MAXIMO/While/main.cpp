d03857a701ead9322da13fb65fddc915801655f5
AlanGomezPasillas <alangomezpasillas@gmail.com>
2025-02-20 16:05:22 -0600

    Update main.cpp

--- A01/c.-estructurado-iteracion/5.-MAXIMO/While/main.cpp
+++ A01/c.-estructurado-iteracion/5.-MAXIMO/While/main.cpp
@@ -5,12 +5,12 @@ int main(){
 	int max = 0;
 	int cant = 5;
     
-    while(cant--){
-        int num;
+	while(cant--){
+		int num;
 		cout<<"Ingresa un numero: ";
 		cin>>num;
-        if(num>max) max=num;
-    }
+		if(num>max) max=num;
+	}
 
 	cout<<"El valor maximo es: "<<max<<endl;
 
