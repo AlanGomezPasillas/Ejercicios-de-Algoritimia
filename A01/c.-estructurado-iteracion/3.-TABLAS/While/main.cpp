4c2f2510c0b770c2e0cf9a52ed98b67ed74ed4b1
AlanGomezPasillas <alangomezpasillas@gmail.com>
2025-02-20 12:42:11 -0600

    Update main.cpp

--- A01/c.-estructurado-iteracion/3.-TABLAS/While/main.cpp
+++ A01/c.-estructurado-iteracion/3.-TABLAS/While/main.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+using namespace std;
 
+int main(){
+	int tabla, veces;
+
+	cout<<"-- Tabla de multiplicar --\n";
+	cout<<"--------------------------\n\n";
+	cout<<"Ingresa la tabla a realizar: ";
+	cin>>tabla;
+	cout<<"Ingresa el numero de iteraciones: ";
+	cin>>veces;
+    
+    int i=1;
+	while(veces--){
+		cout<<tabla<<" * "<<i<<" = "<<tabla*i<<endl;
+		i++;
+	}
+
+	return 0;
+}
