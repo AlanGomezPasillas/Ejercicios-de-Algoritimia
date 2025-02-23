a2f3f5f9683ba4e63708deed8e61afb4dc4972bb
Alan <alangomezpasillas@gmail.com>
2025-02-23 09:23:44 +0000

    a.-FuerzaBruta1 Terminado

--- A02/P01/a.-FuerzaBruta1.cpp
+++ A02/P01/a.-FuerzaBruta1.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <string>
+#include <ctime>
 using namespace std;
 
-bool isPrime(int num) {
+string formatTime(long double t) {
+	ostringstream time;
+	int hour, min, sec, milisec;
+	
+	hour = t/3600;
+	t = t - (hour*3600);
+	min = t/60;
+	t = t - (min*60);
+	sec = t;
+	t = t - (sec/1);
+	milisec = t*10000;
+
+	time << setfill('0') <<
+		setw(2) << hour << ':' <<
+		setw(2) << min << ':' <<
+		setw(2) << sec << '.' << milisec;
+	
+	return time.str();
+}
+
+bool isPrime(long long num) {
 	bool res = true;
-	for(int i = 2; i < num; i++){
+	for(long long i = 2; i < num; i++){
 		if (num % i == 0) res = false;
 	}
 	
@@ -11,16 +35,22 @@ bool isPrime(int num) {
 }
 
 int main() {
-	int number;
+	long long number;
 	bool prime;
+	clock_t t;
+	string time;
 	
 	cin >> number;
-
-	prime = isPrime(number);
 	
+	t = clock();
+	prime = isPrime(number);
+	t = clock() - t;
+	time = formatTime(((long double)t)/CLOCKS_PER_SEC);
+
 	cout << number << ' ';
 	prime? cout << "si" : cout << "no";
 	cout << " es primo." << endl;
+	cout << "Tiempo: " << time << endl;
 
 	return 0;
 }
