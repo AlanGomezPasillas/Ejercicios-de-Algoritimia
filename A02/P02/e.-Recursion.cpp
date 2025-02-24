8c1af037fcae7f16b27832027f840d997c03ddd6
Alan <alangomezpasillas@gmail.com>
2025-02-24 08:40:49 +0000

    Proyecto 1 Terminado

---
+++ A02/P02/e.-Recursion.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <string>
+#include <ctime>
+#include <cmath>
+using namespace std;
+
+string formatTime(long double t) {
+	ostringstream time;
+	int hour, min, sec, milisec;
+	
+	hour = t/3600;
+	t = t - (hour*3600);
+	min = t/60;
+	t = t - (min*60);
+	sec = t;
+	t = t - (sec/1);
+	milisec = t*10000;
+
+	time << setfill('0') <<
+		setw(2) << hour << ':' <<
+		setw(2) << min << ':' <<
+		setw(2) << sec << '.' << 
+		setw(4) << milisec;
+	
+	return time.str();
+}
+
+bool isPrime(long long num, long long i = 2) {
+	/*for(long long i = 2; i < sqrt(num); i++){
+		if (num % i == 0) return false;
+	}*/
+	//cout << num << ' ' << i << endl;
+
+	if (num == 2) return true;
+	if (i > sqrt(num)) return true;
+	if (num % i == 0) return false;
+
+	return isPrime(num, i+1);
+}
+
+int main() {
+	long long number;
+	int primes = 0;
+	clock_t t;
+	string time;
+	
+	t = clock();
+	while(1){
+		cin >> number;
+		if (number == -1) break;
+		primes += isPrime(number);
+	}
+	t = clock() - t;
+	
+	time = formatTime(((long double)t)/CLOCKS_PER_SEC);
+
+	cout << "Total: " << primes << endl;
+	cout << "Tiempo: " << time << endl;
+
+	return 0;
+}
---
+++ A02/P02/f.-CribaEratostenes.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <algorithm>
+#include <iomanip>
+#include <sstream>
+#include <string>
+#include <vector> 
+#include <ctime>
+#include <cmath>
+using namespace std;
+
+string formatTime(long double t) {
+	ostringstream time;
+	int hour, min, sec, milisec;
+	
+	hour = t/3600;
+	t = t - (hour*3600);
+	min = t/60;
+	t = t - (min*60);
+	sec = t;
+	t = t - (sec/1);
+	milisec = t*10000;
+
+	time << setfill('0') <<
+		setw(2) << hour << ':' <<
+		setw(2) << min << ':' <<
+		setw(2) << sec << '.' << 
+		setw(4) << milisec;
+	
+	return time.str();
+}
+
+void criba(long long num1, long long num2, vector<bool> &primes){
+	primes = vector<bool>(num2 - num1, true);
+	for(long long i = 2; i <= sqrt(num2); i++) {
+		for (long long j = i; j*i <= num2; j++) {
+			primes[(i*j)-num1]=0;
+		}
+	}
+}
+
+bool isPrime(long long num, vector<bool> primes){
+	return primes.at(num);
+}
+
+int main() {
+	vector<bool> primes;
+	vector<long long> numbers;
+	long long number, min, max;
+	int nPrimes = 0;
+	clock_t t;
+	string time;
+	
+	t = clock();
+	while(1){
+		cin >> number;
+		if (number == -1) break;
+		numbers.push_back(number);
+	}
+	min = *min_element(numbers.begin(), numbers.end());
+	max = *max_element(numbers.begin(), numbers.end());
+	criba(min, max+1, primes);
+	for(long long i = 0; i < numbers.size(); i++){
+		nPrimes += isPrime(numbers.at(i)-min, primes);
+	}
+	t = clock() - t;
+
+	/*for(int i = 0; i < primes.size(); i++) 
+		cout << primes.at(i) << " ";
+	cout << endl;*/
+
+	time = formatTime(((long double)t)/CLOCKS_PER_SEC);
+
+	cout << "Total: " << nPrimes << endl;
+	cout << "Tiempo: " << time << endl;
+
+	return 0;
+}
