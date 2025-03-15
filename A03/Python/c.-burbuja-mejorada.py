6133b085ce2e75f103cc3c5df17f7a77f0d4bea2
Alan <alangomezpasillas@gmail.com>
2025-03-15 04:57:28 +0000

    Python burbuja terminados

--- A03/Python/c.-burbuja-mejorada.py
+++ A03/Python/c.-burbuja-mejorada.py
@@ -23,11 +23,14 @@ def format_time(t):
 
 def bubble_sort(n, nums):
     for i in range(n-1):
-        for j in range(n-1):
+        swapped = False
+        for j in range(n-i-1):
             if(nums[j] > nums[j+1]):
                 nums[j+1] = nums[j] ^ nums[j+1]
                 nums[j] = nums[j] ^ nums[j+1]
                 nums[j+1] = nums[j] ^ nums[j+1]
+                swapped = True
+        if not swapped: break
 
 
 def main():
