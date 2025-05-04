59534b9300349de56bd78a6862e33f0b9ba1b76c
Alan <alangomezpasillas@gmail.com>
2025-05-04 03:54:18 +0000

    Avanzando en la parte 2

--- A04/C++/P01/construye-grafo.cpp
+++ A04/C++/P01/construye-grafo.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <string>
+#include <ctime>
 #include <map>
 using namespace std;
+
+string formatTime(long double t) {
+    ostringstream time;
+    int hour, min, sec, milisec;
+
+    hour = t/3600;
+    t = t - (hour*3600);
+    min = t/60;
+    t = t - (min*60);
+    sec = t;
+    t = t - (sec/1);
+    milisec = t*10000;
+
+    time << setfill('0') <<
+	    setw(2) << hour << ':' <<
+	    setw(2) << min << ':' <<
+	    setw(2) << sec << '.' <<
+	    setw(4) << milisec;
+
+    return time.str();
+}
 
 void fillMatrix(char mat[], int n1, int n2, char content) {
     for (int i = 0; i < n1; i++) {
@@ -26,11 +51,13 @@ void printMatrix(char mat[], int n1, int n2, map<int, char> &nodes) {
 };*/
 
 int main() {
+    time_t t;
     int it = 0;
     int nNodes;
     int nMatches;
     map<int, char> nodes;
     multimap<int, int> matches;
+    string time;
 
     cin >> nNodes;
     //node myNodes[nNodes];
@@ -54,6 +81,7 @@ int main() {
 	matches.insert({a, b});
     }
     
+    t = clock();
     for (auto x: matches) {
 	adjMatrix[x.first-1][x.second-1] = '1';
 	adjMatrix[x.second-1][x.first-1] = '1';
@@ -61,10 +89,12 @@ int main() {
 	incMatrix[x.second-1][it] = '1';
 	it++;
     }
+    t = clock() - t;
+    time = formatTime(((long double)t)/CLOCKS_PER_SEC);
 
     cout << "Matriz de Adyacencia:" << endl << " ";
     for (int i = 0; i < nNodes; i++) {
-	cout << nodes[i];
+	cout << nodes[i+1];
     }
     cout << endl;
     printMatrix((char*)adjMatrix, nNodes, nNodes, nodes);
@@ -75,6 +105,8 @@ int main() {
     }
     cout << endl;
     printMatrix((char*)incMatrix, nNodes, nMatches, nodes);
+    
+    cout << endl << "Tiempo: " << time << endl;
 
     return 0;
 }
---
+++ A04/C++/P02/creanodos.cpp
@@ -0,0 +1,31 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main() {
+    string name;
+    vector<string> names;
+
+    while (1) {
+	getline(cin, name);
+	if (name == "-1") break;
+	if (names.size() > 0) {
+	    auto res = find(names.begin(), names.end(), name);
+	    if (*res != name){
+		names.push_back(name);
+	    } else {
+		cout << "El nombre ya existe." << endl;
+	    }
+	} else {
+	    names.push_back(name);
+	}
+    }
+
+    for(int i = 0; i < names.size(); i++) {
+	cout << i+1 << " " << names.at(i) << endl;
+    }
+
+    return 0;
+}
